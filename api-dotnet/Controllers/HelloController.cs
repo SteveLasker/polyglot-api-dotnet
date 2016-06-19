4779c887aa620c80c31c3ef9473f3040b22eea81
Steve Lasker <StevenLasker@Hotmail.com>
2016-06-18 17:38:45 -0700

    Add TzInfo

--- api-dotnet/Controllers/HelloController.cs
+++ api-dotnet/Controllers/HelloController.cs
@@ -13,8 +13,24 @@ namespace ApiDotNet.Controllers
             var osDescription = RuntimeInformation.OSDescription;
             var machineName = Environment.MachineName;
 
-            var message = string.Format("Hello from api-dotnet running on: {1}", 
-                osDescription);
+            // TimeZoneInfo has unique Ids per OS
+            // Querying the wrong string will throw
+            TimeZoneInfo tzInfo = null;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                tzInfo = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                tzInfo = TimeZoneInfo.FindSystemTimeZoneById("America/Los_Angeles");
+
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                tzInfo = TimeZoneInfo.FindSystemTimeZoneById("America/Los_Angeles");
+
+            else
+                tzInfo = TimeZoneInfo.FindSystemTimeZoneById(TimeZoneInfo.Local.Id);
+
+            var message = string.Format("Hello from api-dotnet. running on: {0} {1}", 
+                osDescription,
+                tzInfo.ToString());
 
             return message;
         }
